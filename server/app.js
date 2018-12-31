54bce874ce7bbaea2058d88650cb64f5240d7020
Sajid Zaman <sajid.zaman@zeropoint.it>
2018-12-31 18:55:07 +0500

    adds social authentication with passportjs

--- server/app.js
+++ server/app.js
@@ -2,7 +2,11 @@ const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
 const mongoose = require("mongoose");
-
+const authRoutes = require("./routes/auth-routes");
+const profileRoutes = require("./routes/profile-routes");
+const passportSetup = require("./config/passport-setup");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 const app = express();
 
 mongoose.connect(
@@ -12,6 +16,28 @@ mongoose.connect(
 mongoose.connection.once("open", () => {
   console.log("connected to database");
 });
+
+app.set("view engine", "ejs");
+
+app.use(
+  cookieSession({
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: ["thenetninjaisawesomenodoubt"]
+  })
+);
+
+//initialize passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get("/", (req, res) => {
+  res.render("home", { user: req.user });
+});
+
+//setup auth routes
+app.use("/auth", authRoutes);
+app.use("/profile", profileRoutes);
+
 app.use(
   "/graphql",
   graphqlHTTP({
